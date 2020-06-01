ea45adc93d5898ad6998453c3a857e89dae3ca56
Diego Alvarado <diego.alvarado@wolvenhall.com>
2020-06-01 14:22:10 +0000

    API_v1

---
+++ api_jsonrpc/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "api_jsonrpc",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        API JSON RPC
+    """,
+    "category": "Tools",
+    'author': "Wolvenhall",
+    'website': "http://www.wolvenhall.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+    "data": [],
+    "test": [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
