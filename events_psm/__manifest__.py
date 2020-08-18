a354dd8f6867f3f233736f6fa6c7cbbfbd1cb709
Diego Alvarado <diego.alvarado@wolvenhall.com>
2020-08-18 19:05:28 +0000

    [ADD] Eventos PSM: Nuevo modulo

---
+++ events_psm/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Eventos PSM",
+
+    'summary': """Eventos Promotora Socia México""",
+
+    'description': """Eventos Promotora Socia México""",
+    "category": "Tools",
+    'author': "Wolvenhall",
+    'website': "http://www.wolvenhall.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'approvals'],
+    "data": [],
+    "test": [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
