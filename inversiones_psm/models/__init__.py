8206fe394c3dc6ff2ef42a5766280789b0c6d7e8
Diego Alvarado <diego.alvarado@wolvenhall.com>
2020-10-19 18:52:56 +0000

    [DELETE] Modulos de inversiones y egresos

--- inversiones_psm/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import cedula_asociadas, cedula_acciones, cedula_fondos, creditos_lineas, creditos_tipo, creditos_devengo, \
-    creditos_amortizacion, creditos_disposicion, portafolio
